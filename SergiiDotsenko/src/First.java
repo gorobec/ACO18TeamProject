592e456816aa63a08be468f3fa491010ef8fe437
SDotsenko <serdotsenko@gmail.com>
2017-01-29 22:34:09 +0200

    library draft ready

--- SergiiDotsenko/src/First.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by SDotsenko on 29.01.2017.
- */
-public class First {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
---
+++ SergiiDotsenko/src/library/common/publication/Publication.java
@@ -0,0 +1,7 @@
+package library.common.publication;
+
+public interface Publication {
+    PublicationType getPublicationType();
+    void printPublicationType();
+    void printFullInfo();
+}
