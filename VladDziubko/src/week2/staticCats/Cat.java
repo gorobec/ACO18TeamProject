3a31951ac9ca574b0ef301845af1fcfdf35a0ada
Reed17 <777reed777@gmail.com>
2017-02-01 19:41:23 +0200

    static cats task

--- VladDziubko/src/week2/staticCats/Cat.java
+++ VladDziubko/src/week2/staticCats/Cat.java
@@ -1,4 +1,4 @@
-package week2.homework;
+package week2.staticCats;
 
 import java.util.ArrayList;
 
--- VladDziubko/src/week2/staticCats/Run.java
+++ VladDziubko/src/week2/staticCats/Run.java
@@ -1,4 +1,4 @@
-package week2.homework;
+package week2.staticCats;
 
 public class Run {
     public static void main(String[] args) {
