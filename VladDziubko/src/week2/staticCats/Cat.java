b725eb39760987a6a5c8b314a3912a77ad3aa57d
Reed17 <777reed777@gmail.com>
2017-02-01 19:35:29 +0200

    static cats task

---
+++ VladDziubko/src/week2/staticCats/Cat.java
@@ -0,0 +1,36 @@
+package week2.homework;
+
+import java.util.ArrayList;
+
+public class Cat {
+    private String name;
+    private int age;
+    public static ArrayList<Cat> cats = new ArrayList<>();
+
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+        cats.add(this);
+    }
+
+    public Cat(){
+        name = "Some cat";
+        age = 5;
+        cats.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static ArrayList<Cat> printCats(){
+        for (Cat cat : cats) {
+            System.out.printf("%s, age -> %d\n", cat.getName(), cat.getAge());
+        }
+        return cats;
+    }
+}
---
+++ VladDziubko/src/week2/staticCats/Run.java
@@ -0,0 +1,18 @@
+package week2.homework;
+
+public class Run {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Vas`ka", 2);
+        Cat cat2 = new Cat("Murka", 9);
+        Cat cat3 = new Cat("Lord", 10);
+        Cat cat4 = new Cat("Graf", 1);
+        Cat cat5 = new Cat("Mira", 8);
+        Cat cat6 = new Cat("Laiza", 7);
+        Cat cat7 = new Cat("Murzilka", 6);
+        Cat cat8 = new Cat("Rizhik", 5);
+        Cat cat9 = new Cat("Zoia", 4);
+        Cat cat10 = new Cat("Violetta", 3);
+        Cat.printCats();
+
+    }
+}
