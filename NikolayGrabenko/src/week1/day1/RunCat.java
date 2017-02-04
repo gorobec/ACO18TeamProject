555006cfcdb72d50f350d28fa37845e0aa3319ba
pantey86 <pantey86@mail.ru>
2017-02-04 20:53:13 +0200

    static Cats with RunCats

---
+++ NikolayGrabenko/src/week1/day1/RunCat.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class RunCat {
+
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat("Vasiliy", "Grey");
+        Cat cat2 = new Cat("Borys", "Black");
+        Cat cat3 = new Cat("Bars", "Grey");
+        Cat cat4 = new Cat("Silver", "Black");
+        Cat cat5 = new Cat("Snow", "White");
+        Cat cat6 = new Cat("Mursik", "Grey");
+        Cat cat7 = new Cat("Pushok", "White");
+        Cat cat8 = new Cat("Storm", "Red");
+        Cat cat9 = new Cat("Fox", "Red");
+        Cat cat10 = new Cat("Vasya", "Grey-White");
+
+        Cat.printCats(Cat.cats);
+
+    }
+}
