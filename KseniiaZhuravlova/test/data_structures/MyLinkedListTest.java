58c1fad33ecda6421d0d0ff9cbddfbc952bf24c5
ksyashka <ksyashka19@gmail.com>
2017-02-04 16:15:26 +0200

    test

--- KseniiaZhuravlova/test/data_structures/MyLinkedListTest.java
+++ KseniiaZhuravlova/test/data_structures/MyLinkedListTest.java
@@ -3,5 +3,9 @@ package data_structures;
 /**
  * Created by ksyashka on 04.02.2017.
  */
-public class MyLinkedListTest {
+public class MyLinkedListTest extends MyListTest {
+    @Override
+    public void setUp() {
+        list = new MyLinkedList<String>();
+    }
 }
