3fe7d0f726a1b2b524728a57c5df7d104a0fe1d9
Nastia <Nastia@AnastasiiaShvetsova.local>
2017-02-05 11:30:06 +0200

    fixed enums

--- AnastasiiaShvetsova/src/library/model/enums/BookGenre.java
+++ AnastasiiaShvetsova/src/library/model/enums/BookGenre.java
@@ -4,13 +4,13 @@ package library.model.enums;
  * Created by anastasiia.shvetsova on 2/3/2017.
  */
 public enum BookGenre {
-    Comedy,
-    Drama,
-    Horror,
-    History,
-    War,
-    Family,
-    Adventure,
-    Animation,
-    Mystery
+    COMEDY,
+    DRAMA,
+    HORROR,
+    HISTORY,
+    WAR,
+    FAMILY,
+    ADVENTURE,
+    ANIMATION,
+    MYSTERY
 }
--- AnastasiiaShvetsova/src/library/model/enums/MagazineGenre.java
+++ AnastasiiaShvetsova/src/library/model/enums/MagazineGenre.java
@@ -4,8 +4,8 @@ package library.model.enums;
  * Created by anastasiia.shvetsova on 2/3/2017.
  */
 public enum MagazineGenre {
-    Countryside,
-    Sport,
-    Cookery,
-    Fashion
+    COUNTRYSIDE,
+    SPORT,
+    COOKERY,
+    FASHION
 }
