3e60783254395c407ff840093066e134876a985c
gorobec <yevhenijvorobiei@gmail.com>
2017-02-05 10:45:18 +0200

    week 3 day 1

---
+++ YevhenVorobiei/src/week3/day1/generics/TestSomeType.java
@@ -0,0 +1,20 @@
+package week3.day1.generics;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by gorobec on 05.02.17.
+ */
+public class TestSomeType {
+    public static void main(String[] args) {
+        SomeType<?> someType = new SomeType();
+
+        List<String> list = new LinkedList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        someType.print(list);
+    }
+}
