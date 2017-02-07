5dcb5070ab43c8c77f82f1573dc37f0a1069e57b
v21k <vlad.kornieiev@gmail.com>
2017-02-07 10:23:12 +0200

    clone LL_tast

---
+++ VladKornieiev/src/ACO/week3/homework/main/java/ua/com/artcode/A.java
@@ -0,0 +1,6 @@
+package ACO.week3.homework.main.java.ua.com.artcode;
+
+public interface A<T> {
+    void setNext(A<T> next);
+    A<T> getNext();
+}
---
+++ VladKornieiev/src/ACO/week3/homework/main/java/ua/com/artcode/ListUtils.java
@@ -0,0 +1,28 @@
+package ACO.week3.homework.main.java.ua.com.artcode;
+
+public class ListUtils {
+
+    public static A<String> createStringList() {
+        A<String> head = new Node<>("A");
+        A<String> second = new Node<>("B");
+        A<String> third = new Node<>("C");
+        A<String> fourth = new Node<>("D");
+        A<String> fifth = new Node<>("E");
+        A<String> sixth = new Node<>("F");
+        A<String> seventh = new Node<>("G");
+        head.setNext(second);
+        second.setNext(third);
+        third.setNext(fourth);
+        fourth.setNext(fifth);
+        fifth.setNext(sixth);
+        sixth.setNext(seventh);
+        return head;
+    }
+
+    public static<T> A<T> reversion(A<T> head) {
+
+
+        return head;
+
+    }
+}
