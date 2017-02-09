bc67e6fb88eb0c39a1b58c5ae53978c77df841c6
v21k <vlad.kornieiev@gmail.com>
2017-02-09 09:56:48 +0200

    fix branch

---
+++ VladKornieiev/test/ACO/data_structures/MyLinkedListTest.java
@@ -0,0 +1,15 @@
+package ACO.data_structures;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by v21k on 06.02.17.
+ */
+public class MyLinkedListTest extends MyListTest {
+    @Override
+    public void setUp() {
+        list = new MyLinkedList<>();
+    }
+
+
+}
