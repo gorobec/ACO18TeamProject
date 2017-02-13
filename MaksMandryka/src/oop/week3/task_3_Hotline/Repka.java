decaf31e05a8d594f00fe2e7d41c0e0622229726
Администратор <makspman@gmail.com>
2017-02-13 23:50:54 +0200

    hotline

---
+++ MaksMandryka/src/oop/week3/task_3_Hotline/Repka.java
@@ -0,0 +1,11 @@
+package oop.week3.task_3_Hotline;
+
+import java.util.List;
+
+/**
+ * Created by Администратор on 13.02.2017.
+ */
+public class Rozetka {
+
+    List<Product> products;
+}
---
+++ MaksMandryka/test/oop/week3/task_3_Hotline/TestHotline.java
@@ -0,0 +1,7 @@
+package oop.week3.task_3_Hotline;
+
+/**
+ * Created by Администратор on 13.02.2017.
+ */
+public class TestHotline {
+}
