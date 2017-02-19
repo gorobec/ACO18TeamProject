9c92de2ce07f2fb47ca46762a8cbb9c825a00a97
pantey86 <pantey86@mail.ru>
2017-02-19 12:52:45 +0200

    no changes

--- YevhenVorobiei/test/data_structures/MyArrayListTest.java
+++ YevhenVorobiei/test/data_structures/MyArrayListTest.java
@@ -1,6 +1,9 @@
 package data_structures;
 
 import org.junit.*;
+
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.After;
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.*;
 
 /**
