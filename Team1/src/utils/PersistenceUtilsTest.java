968bc81965ed93b12be41ad06a0d35b206354101
presly808 <presly808@gmail.com>
2017-02-19 14:12:07 +0200

    added one test

--- Team1/src/utils/PersistenceUtilsTest.java
+++ Team1/src/utils/PersistenceUtilsTest.java
@@ -1,6 +1,11 @@
 package utils;
 
+import controller.DB;
+import model.Address;
+import model.Product;
+import model.Ticket;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,11 +19,19 @@ public class PersistenceUtilsTest {
 
 
     private String json;
+    private DB db;
 
 
     @Before
     public void setUp() throws Exception {
+        db = new DB();
+        db.addProduct(new Product(1,"ProdName1"));
+        db.addProduct(new Product(2,"ProdName2"));
+        db.addProduct(new Product(1,"ProdName3"));
 
+        db.addTicket(new Ticket(1, "credCard", new Address("kiev", "sdf", 12)));
+        db.addTicket(new Ticket(2, "credCard", new Address("kiev", "sdf", 12)));
+        db.addTicket(new Ticket(3, "credCard", new Address("kiev","sdf", 12)));
     }
 
     @After
@@ -28,12 +41,14 @@ public class PersistenceUtilsTest {
 
     @Test
     public void testConvertToJson() throws Exception {
-
+        String json = PersistenceUtils.convertToJson(db);
+        System.out.println(json);
+        Assert.assertNotNull(json);
     }
 
     @Test
     public void testConvertFromJson() throws Exception {
-
+//        PersistenceUtils.convertFromJson(DB.class,)
     }
 
     @Test
