dd93a578708168abc1e4c3e5bfdc71431e38569c
v21k <vlad.kornieiev@gmail.com>
2017-02-19 12:55:38 +0200

    team1

--- Team1/src/controller/IService.java
+++ Team1/src/controller/IService.java
@@ -1,7 +1,19 @@
 package controller;
 
+import exception.InvalidIdException;
+import exception.NoSuchProductException;
+import model.Address;
+import model.Product;
+import model.Ticket;
+
+import java.util.List;
+
 /**
  * Created by v21k on 19.02.17.
  */
 public interface IService {
+    List<Product> getProducts();
+    Product getProductById(int id) throws InvalidIdException;
+    Ticket buy(Product product, Address address, String creditCard) throws NoSuchProductException;
+    Ticket showTicket(int id) throws InvalidIdException;
 }
---
+++ Team1/src/view/ViewUtils.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * Created by v21k on 19.02.17.
+ */
+public class ViewUtils {
+}
