e9ef22ea5fb0ba8100322eb09d7d9d68a964e385
Игорь <edoga @ukr.net>
2017-02-19 13:42:28 +0200

    IDataBase

--- Team1/src/controller/IDataBase.java
+++ Team1/src/controller/IDataBase.java
@@ -9,10 +9,11 @@ import java.util.List;
  * Created by Игорь on 19.02.2017.
  */
 public interface IDataBase {
-    void addTicket(Ticket ticket);
-    void addProduct(Product product);
-    void removeProductByID(int id);
-    void removeTicketByID(int id);
+
+    boolean addTicket(Ticket ticket);
+    boolean addProduct(Product product);
+    boolean removeProductByID(int id);
+    boolean removeTicketByID(int id);
     Product getProductByID(int id);
     Ticket getTicketByID(int id);
     List<Product> getAllProduct();
