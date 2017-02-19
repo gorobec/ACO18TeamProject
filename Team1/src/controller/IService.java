cb02e755b291f0a00c6277e6483989da641c54f4
SDotsenko <serdotsenko@gmail.com>
2017-02-19 13:54:41 +0200

    controller ready

--- Team1/src/controller/IService.java
+++ Team1/src/controller/IService.java
@@ -12,10 +12,20 @@ import java.util.List;
  * Created by v21k on 19.02.17.
  */
 public interface IService {
-    Product addProduct(Product product);
-    Ticket addTicket(Ticket ticket);
+    boolean addProduct(Product product);
+
+    boolean addTicket(Ticket ticket);
+
     List<Product> getProducts();
+
     Product getProductById(int id) throws InvalidIdException;
-    Ticket buy(Product product, Address address, String creditCard) throws NoSuchProductException;
+
+    Ticket getTicketById(int id) throws InvalidIdException;
+
+    boolean buy(int productID, Address address, String creditCard) throws NoSuchProductException;
+
     Ticket showTicket(int id) throws InvalidIdException;
+
+    boolean addPoduct(Product product);
+
 }
