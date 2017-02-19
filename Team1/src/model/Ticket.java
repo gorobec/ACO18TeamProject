dabca5f1c3c0dbdfa7a4b4c6d0ccbf330be0065d
v21k <vlad.kornieiev@gmail.com>
2017-02-19 12:45:10 +0200

    team1

---
+++ Team1/src/model/Ticket.java
@@ -0,0 +1,59 @@
+package model;
+
+/**
+ * Created by v21k on 19.02.17.
+ */
+public class Ticket {
+    private int id;
+    private String creditCard;
+    private Address address;
+
+    public Ticket() {
+    }
+
+
+    public Ticket(int id, String creditCard, Address address) {
+        this.id = id;
+        this.creditCard = creditCard;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(String creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ticket ticket = (Ticket) o;
+
+        return id == ticket.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
---
+++ VladKornieiev/test/ACO/week5/IBashTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by v21k on 18.02.17.
+ */
+public class IBashTest {
+
+}
