d1bf2aeda448d35d29c73418a472d7ca9767d04d
Maks M <makspman@gmail.com>
2017-02-19 14:02:05 +0200

    tests

---
+++ BestBuy/src/exceptions/NoSuchProductException.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by fmandryka on 19.02.2017.
+ */
+public class NoSuchProductException {
+}
---
+++ BestBuy/src/model/DataBase.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by fmandryka on 19.02.2017.
+ */
+public class DataBase {
+}
