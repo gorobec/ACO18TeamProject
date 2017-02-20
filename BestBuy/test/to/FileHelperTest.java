bc93dc6f8f41ba1e027e0fe38e494b016b75221b
Администратор <makspman@gmail.com>
2017-02-20 21:04:30 +0200

    test

--- BestBuy/test/to/FileHelperTest.java
+++ BestBuy/test/to/FileHelperTest.java
@@ -12,16 +12,14 @@ public class FileHelperTest {
     private FileHelper fileHelper = new FileHelper();
     @org.junit.Test
     public void readFromFile() throws Exception {
-        String str = fileHelper.readFromFile("C:/Users/ksyashka/IdeaProjects/" +
-                "ACO18TeamProject/BestBuy/resources/test.txt");
+        String str = fileHelper.readFromFile("resources/test.txt");
         Assert.assertEquals(str, "Hello World! 1234466");
 
     }
 
     @org.junit.Test
     public void writeToFile() throws Exception {
-        Assert.assertTrue(fileHelper.writeToFile("Hello World! 1234466","C:/Users/ksyashka/IdeaProjects/" +
-                "ACO18TeamProject/BestBuy/resources/test.txt"));
+        Assert.assertTrue(fileHelper.writeToFile("Hello World! 1234466","resources/test.txt"));
     }
 
 }
