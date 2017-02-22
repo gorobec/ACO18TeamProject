d8e5f9af4462b96d4c87d90498bd22c13d2a189a
v21k <vlad.kornieiev@gmail.com>
2017-02-22 20:09:27 +0200

    added getter/setter

--- Team1/src/model/User.java
+++ Team1/src/model/User.java
@@ -35,6 +35,14 @@ public class User {
         return email;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
