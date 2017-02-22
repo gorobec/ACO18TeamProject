dace7bcf3364ceef7a4f7e7b9c7712e3e44b4635
pantey86 <pantey86@mail.ru>
2017-02-22 16:28:52 +0200

    IStore methods added

--- BestBuy/src/controller/IStore.java
+++ BestBuy/src/controller/IStore.java
@@ -17,16 +17,37 @@ public interface IStore {
     Map<Integer, Product> getAllProducts();
 
     Product getProductById(int id) throws NoSuchProductException;
-
+//same as addProductByIdToTicket (i cant put User Obj: how to print info about user? it must be some user ID? )
     Ticket buy(User user, int productId);
-
+    // we dont need it in View
     Ticket showTicketById(int id) throws NoSuchTicketException;
-
+    // we dont need it in View
     String printTicketById(Ticket ticket);
-
+    // we dont need it in View but i can create such button (i think- its not users" option)
     String printAllTickets();
-
+    // we dont need it in View
     String printProduct(Product product);
-
+// button - "show all products in catalog "
     String printAllProducts();
+
+//// button - "OK " in login Window
+//    boolean checkLoginAndPassword (String login, String password); /*throws UserNotRegisteredException, IllegalPasswordException*/;
+// //button - "Apply " in Registration Window
+//    boolean registerUser (String email, String password, String address, int creditCard); /*throws IllegalEmailFormatException,
+//    UserWithSuchEmailRegisteredException,IllegalPasswordFormatException,IllegalCreditCardFormatException */
+
+//    String printTicketById(int id)  throws NoSuchTicketException;
+
+//    String printProductById(int id)  throws NoSuchProductException;
+
+
+// //   create new Ticket with product ID
+//   (boolean or Ticket?) addProductByIdToTicket (int productId)throws NoSuchProductException;
+
+//  boolean removeProductByIdFromTicket (int productId)throws NoSuchProductException;
+    // send Ticket to DB
+//    buy () throws TicketIsEmptyException(but its impossible, we have to create TicketBuilder
+// to create empy Ticket without IDproduct and then after pushing addProduct button we can add IDproduct );
+
+
 }
