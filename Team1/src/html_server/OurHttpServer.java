001334d9dd83334ad39ce698550139c311d37ad8
v21k <vlad.kornieiev@gmail.com>
2017-02-25 21:21:15 +0200

    HttpServer added

--- Team1/src/html_server/OurHttpServer.java
+++ Team1/src/html_server/OurHttpServer.java
@@ -11,15 +11,14 @@ import java.net.InetSocketAddress;
  * Created by v21k on 25.02.17.
  */
 public class OurHttpServer {
-    public static HttpServer httpServer;
     public static String token;
 
     public static void main(String[] args) throws IOException {
         // load from db
         IService iService = ViewUtils.load();
 
-        // create server and create various contexts
-        httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+        // create server and various contexts
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
         LoginContextCreator.createLoginContext(httpServer, iService);
         RegisterContextCreator.createRegisterContext(httpServer, iService);
         httpServer.start();
