9879b77732c31fd0a43fdd3a03bb9d35f3aa9ddd
v21k <vlad.kornieiev@gmail.com>
2017-02-27 23:29:24 +0200

    added methods

--- Team1/src/utils/HttpServerUtils.java
+++ Team1/src/utils/HttpServerUtils.java
@@ -1,10 +1,13 @@
 package utils;
 
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import controller.IService;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 
 /**
  * Created by v21k on 25.02.17.
@@ -31,5 +34,18 @@ public class HttpServerUtils {
 
     public static String[] getParams(String string, String regExpr) {
         return string.split(regExpr);
+    }
+
+    public static <T> T getRequestData(HttpExchange httpExchange, Class<T> model) throws IOException {
+        httpExchange.getResponseHeaders().put("Access-Control-Allow-Origin", Arrays.asList("*"));
+        StringBuilder request = new StringBuilder();
+
+        try (InputStream is = httpExchange.getRequestBody()) {
+            int i;
+            while ((i = is.read()) != -1) {
+                request.append((char) i);
+            }
+        }
+        return new Gson().fromJson(request.toString(), model);
     }
 }
