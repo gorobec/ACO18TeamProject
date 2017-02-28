816257f04b030d81dd421a3bf7e63d1c0f50eba1
Nastia <Nastia@AnastasiiaShvetsova.local>
2017-03-01 00:14:58 +0200

    added products to view

--- BestBuy/src/com/bestBuy/view/bestBuyView/js/main.js
+++ BestBuy/src/com/bestBuy/view/bestBuyView/js/main.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
     var dataItems = [];
 
     $(window).load(function() {
-            $.ajax({
-                url: "http://localhost:8000/allProducts",
-                success: function(result) {
-                    dataItems = result;
-                    console.log(dataItems);
-                     $('#productTmpl').tmpl(dataItems).appendTo('.product-one');
 
-                }
-            });
+        $.ajax({
+        	url: "http://localhost:8000/allProducts",
+        	success: function(result) {
+        	dataItems = result;
+        	console.log(JSON.parse(dataItems));
+        	},
+        	complete: function(result){
+        	$('#productTmpl').tmpl(JSON.parse(dataItems)).appendTo('#product-one');
+        	}
+        	});        
 
-        });
+    });
 
 });
+
