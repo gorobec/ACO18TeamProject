5658bff844215c6b2552fb681b971c4c76dbc956
Nastia <Nastia@AnastasiiaShvetsova.local>
2017-02-28 22:59:35 +0200

    fixed template

--- BestBuy/src/com/bestBuy/view/bestBuyView/js/main.js
+++ BestBuy/src/com/bestBuy/view/bestBuyView/js/main.js
@@ -1,31 +1,15 @@
 $(document).ready(function() {
     var dataItems = [];
+    console.log(1);
     $(window).load(function() {
             $.ajax({
                 url: "http://localhost:8000/allProducts",
                 success: function(result) {
                     dataItems = result;
+                    console.log(dataItems);
                 }
             });
+            $('#productTmpl').tmpl(dataItems).appendTo('.product-one');
         });
-    <script id="productTmpl" type="text/x-jquery-tmpl">
-            <div class="col-md-3 product-left">
-                <div class="product-main simpleCart_shelfItem">
-                    <a href="single.html" class="mask"><img class="img-responsive zoom-img" src="${imageUrl}" alt="" /></a>
-                    <div class="product-bottom">
-                        <h3>${name}</h3>
-                        <p>Explore Now</p>
-                        <h4><a class="item_add" href="#"><i></i></a> <span class="item_price">$ ${price}</span></h4>
-                    </div>
-                    <div class="srch">
-                        <span>-50%</span>
-                    </div>
-                </div>
-            </div>
-    </script>
-    function () {
-                $('#productTmpl').tmpl(dataItems).appendTo('.product-one');
-            }
 
-
-	});
+});
