f6cc7c9d41c5bcc7dfc3aa5c6a47fb1020d8efbc
Nastia <Nastia@AnastasiiaShvetsova.local>
2017-02-28 23:15:32 +0200

    test version

--- BestBuy/src/com/bestBuy/view/bestBuyView/js/main.js
+++ BestBuy/src/com/bestBuy/view/bestBuyView/js/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
     var dataItems = [];
-    console.log(1);
+
     $(window).load(function() {
             $.ajax({
                 url: "http://localhost:8000/allProducts",
                 success: function(result) {
                     dataItems = result;
                     console.log(dataItems);
+                     $('#productTmpl').tmpl(dataItems).appendTo('.product-one');
+
                 }
             });
-            $('#productTmpl').tmpl(dataItems).appendTo('.product-one');
+
         });
 
 });
