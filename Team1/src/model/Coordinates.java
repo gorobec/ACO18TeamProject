fb0fd96008453b474b73debd64ce8e3e7804235f
v21k <vlad.kornieiev@gmail.com>
2017-03-02 11:40:07 +0200

    added new model Coordinates for google map purposes

---
+++ Team1/src/model/Coordinates.java
@@ -0,0 +1,60 @@
+package model;
+
+/**
+ * Created by v21k on 02.03.17.
+ */
+public class Coordinates {
+    private double lat;
+    private double lng;
+
+    public Coordinates() {
+    }
+
+    public Coordinates(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinates that = (Coordinates) o;
+
+        if (Double.compare(that.lat, lat) != 0) return false;
+        return Double.compare(that.lng, lng) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(lat);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(lng);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Latitude: %f, longitude: %f\n", lat, lng);
+    }
+}
