15a2bab6f8d6521a4709eff00f0a2d95b4ec61ad
Nastia <Nastia@AnastasiiaShvetsova.local>
2017-03-04 00:20:27 +0200

    login best buy

--- BestBuy/src/com/bestBuy/view/bestBuyView/js/main.js
+++ BestBuy/src/com/bestBuy/view/bestBuyView/js/main.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     var dataItems = [];
+    var loginData;
 
     $(window).load(function() {
 
         $.ajax({
         	url: "http://localhost:8000/allProducts",
         	success: function(result) {
-        	dataItems = result;
-        	console.log(JSON.parse(dataItems));
+        	    dataItems = result;
+        	    console.log(JSON.parse(dataItems));
         	},
         	complete: function(result){
                 $('#productTmpl').tmpl(JSON.parse(dataItems)).appendTo('#product-one');
@@ -15,9 +16,38 @@ $(document).ready(function() {
         	}
 
 
-        	});        
+        	});
+
 
     });
+     $('form#loginForm').submit(function(e) {
+
+        form = $(this);
+        var data = form.serializeArray();
+        console.log(data);
+//         data = $.map(data, function(element){
+//           return  element.value;
+//        });
+
+            $.ajax({
+                type: form.attr('method'),
+                url: form.attr('action'),
+                data: data,
+                dataType: JSON,
+                success: function(result)
+                {
+                   loginData = result;
+                   console.log("login data" + loginData);
+                },
+                complete: function(result){
+                    console.log("result login data" + loginData);
+
+                }
+         });
+        e.preventDefault();
+
+     });
+
 
 });
 
