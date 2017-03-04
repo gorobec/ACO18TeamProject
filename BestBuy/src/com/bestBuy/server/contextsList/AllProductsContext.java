be4105c70677fba4b3c71030ee2c9f2431a4b7a5
Nastia <Nastia@AnastasiiaShvetsova.local>
2017-03-04 21:38:20 +0200

    added folder contextsList

--- BestBuy/src/com/bestBuy/server/contextsList/AllProductsContext.java
+++ BestBuy/src/com/bestBuy/server/contextsList/AllProductsContext.java
@@ -1,7 +1,36 @@
 package com.bestBuy.server.contextsList;
 
+import com.bestBuy.model.Product;
+import com.bestBuy.to.Serializer;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+
 /**
  * Created by Nastia on 04.03.17.
  */
-public class NewProductContext {
+public class AllProductsContext {
+     server.createContext("/allProducts", new HttpHandler() {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            httpExchange.getResponseHeaders().put("Access-Control-Allow-Origin", Arrays.asList("*"));
+            String requestUrl = httpExchange.getRequestURI().toString();
+            System.out.println(requestUrl);
+            System.out.println("HTTP method is " + httpExchange.getRequestMethod());
+
+            try (OutputStream outputStream = httpExchange.getResponseBody()) {
+                Product[] products = service.showAllProducts();
+                Serializer serializer = Serializer.getInstance();
+                String json = serializer.convertObjectToJson(products);
+                httpExchange.sendResponseHeaders(200, json.length());
+
+                outputStream.write(json.getBytes());
+                outputStream.flush();
+                outputStream.close();
+            }
+
+        }
+    });
 }
